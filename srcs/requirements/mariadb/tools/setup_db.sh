1806fb23bd812860d4851bb52fc3768b68474bd9
Abderrahman Elkhalil <aelkhali@e2r1p8.1337.ma>
2023-12-16 18:25:55 +0100

    mariadb setup

---
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -0,0 +1,13 @@
+service mariadb start
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE_NAME};"
+
+mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+sleep 1
+
+service mariadb stop
+
+exec mysqld --defaults-file=/etc/mysql/my.cnf
